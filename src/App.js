9b0129cf7422938c9f8613fb79d68c79e083e848
Hossain <arafat.kd99@gmail.com>
2024-09-09 20:33:24 +0100

    Declare Nav component and usee in BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Nav from './Components/Nav';
 import './App.css';
 
 function App() {
   return (
     <Router>
       <div className="container">
+        <Nav />
         <div className="App">
           <Routes>
             <Route path="/" />
