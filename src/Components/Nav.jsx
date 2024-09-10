01671e083074399911a11e7b3372e72bdfddfa0c
Hossain <arafat.kd99@gmail.com>
2024-09-10 20:29:56 +0100

    Add links for menu

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -2,47 +2,54 @@
 import { useState } from 'react';
 
 // COMPONENT
-const Nav = () => {
+function Nav() {
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleMenu = () => {
     setIsOpen(!isOpen);
   };
 
- // RETURN USING TAILWIND CLASSES 
+  // RETURN USING TAILWIND CLASSES
   return (
     <nav>
       <div>
+        {/* HAMBURGER MENU */}
         <div>
-          <button onClick={toggleMenu}>
+          <button onClick={toggleMenu} type="button">
             {isOpen ? (
               <svg>
-                <path> 
-
-                </path>
+                <path />
               </svg>
             ) : (
               <svg>
-                <path>
-
-                </path>
+                <path />
               </svg>
             )}
           </button>
         </div>
 
-        // LINK VISIBLE WHEN MENU IS OPEN
+        {/* LINK VISIBLE WHEN MENU IS OPEN */}
         <div
           className={`${
             isOpen ? 'block' : 'hidden'
           }`}
         >
-        </div>  
+          <a href="/">Home</a>
+          <a href="/ambulance">Ambulance</a>
+          <a href="/emergency">Emergency Contacts</a>
+          <a href="/doctors">Doctors</a>
+          <a href="/clinics">Clinics</a>
+          <a href="/hospitals">Hospitals</a>
+          <a href="/pharmacy">Pharmacy</a>
+          <a href="/blood-bank">Blood Bank</a>
+          <a href="/disclaimer">Disclaimer</a>
+          <a href="/about">About  Us</a>
+          <a href="/contact">Contact Us</a>
+        </div>
       </div>
     </nav>
-  )
-
-};
+  );
+}
 
 // EXPORT
 export default Nav;
