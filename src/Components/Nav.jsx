757fadb65f826d8f07b7c0178912c9ca0ca43efe
Hossain <arafat.kd99@gmail.com>
2024-09-10 20:18:41 +0100

    Create toggle button funcionality

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -9,6 +9,39 @@ const Nav = () => {
     setIsOpen(!isOpen);
   };
 
+ // RETURN USING TAILWIND CLASSES 
+  return (
+    <nav>
+      <div>
+        <div>
+          <button onClick={toggleMenu}>
+            {isOpen ? (
+              <svg>
+                <path> 
+
+                </path>
+              </svg>
+            ) : (
+              <svg>
+                <path>
+
+                </path>
+              </svg>
+            )}
+          </button>
+        </div>
+
+        // LINK VISIBLE WHEN MENU IS OPEN
+        <div
+          className={`${
+            isOpen ? 'block' : 'hidden'
+          }`}
+        >
+        </div>  
+      </div>
+    </nav>
+  )
+
 };
 
 // EXPORT
