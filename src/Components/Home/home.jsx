1aad9b347db8e1c43c60c36636bad5e4a9c5aa74
Hossain <arafat.kd99@gmail.com>
2024-09-11 14:40:04 +0100

    Implement useEffect hook to fetch api url

--- src/Components/Home/home.jsx
+++ src/Components/Home/home.jsx
@@ -1,14 +1,28 @@
 // IMPORT
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 // API URL
 const API_url = 'http://localhost:3000/api/v1/specifications';
 
 // COMPONENT
 const Home = () => {
-  const [ specifications, setSpecifications] = useState([]);
+  const [ data,  setData] = useState([]);
   const [ error, setError] = useState(null);
-};
+
+//  FETCH DATA FROM API using useEffect async await
+useEffect(() => {
+  const fetchData = async () => {
+    try {
+      const response = await fetch(API_url);
+      const data = await response.json();
+      setSpecifications(data);
+    } catch (error) {
+      setError(error);
+    }
+  };
+  fetchData();
+}
+, []);
 
 // EXPORT
 export default Home;
