b7914ac3792e3b61bc60e37d1eeffd940830da7b
Hossain <arafat.kd99@gmail.com>
2024-09-11 14:58:23 +0100

    Return data with mapping

--- src/Components/Home/home.jsx
+++ src/Components/Home/home.jsx
@@ -2,27 +2,45 @@
 import React, { useState, useEffect } from 'react';
 
 // API URL
-const API_url = 'http://localhost:3000/api/v1/specifications';
+const apiUrl = 'http://localhost:3000/api/v1/specifications';
 
 // COMPONENT
 const Home = () => {
-  const [ data,  setData] = useState([]);
-  const [ error, setError] = useState(null);
+  const [data, setData] = useState([]);
+  // const [error, setError] = useState(null);
 
-//  FETCH DATA FROM API using useEffect async await
-useEffect(() => {
-  const fetchData = async () => {
-    try {
-      const response = await fetch(API_url);
-      const data = await response.json();
-      setSpecifications(data);
-    } catch (error) {
-      setError(error);
-    }
-  };
-  fetchData();
-}
-, []);
+  //  FETCH DATA FROM API using useEffect async await
+  useEffect(
+    () => {
+      const fetchData = async () => {
+        try {
+          const response = await fetch(apiUrl);
+          console.log('Response:', response);
+          const data = await response.json();
+          setData(data);
+          console.log(data);
+        } catch (error) {
+          // setError(error);
+        }
+      };
+      fetchData();
+    },
+    [],
+  );
+
+  // RETURN
+  return (
+    <div>
+      <h1>Home</h1>
+      {data.map((item) => (
+        <div key={item.id}>
+          <h2>{item.name}</h2>
+          <p>{item.description}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 // EXPORT
 export default Home;
