089d95c0120fc0ddd590bba9ca59ce605d0ce220
Hossain <arafat.kd99@gmail.com>
2024-09-12 05:13:04 +0100

    Set initial state for DoctorList component

--- src/Components/DoctorList.jsx
+++ src/Components/DoctorList.jsx
@@ -1,2 +1,9 @@
 // IMPORTS
 import React, { useState, useEffect } from 'react';
+
+// DEFINE THE COMPONENT
+const DoctorList = ({ specificationId }) => {
+  // STATE
+  const [doctors, setDoctors] = useState([]);
+  const [error, setError] = useState([]);
+  
