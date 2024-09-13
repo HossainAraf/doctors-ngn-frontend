80ea4fcd47725851745fdd7b815e9c4e9094df3c
Hossain <arafat.kd99@gmail.com>
2024-09-13 03:19:46 +0100

    Add useEffect hook to fetch Doctor list

--- src/Components/DoctorList.jsx
+++ src/Components/DoctorList.jsx
@@ -5,5 +5,20 @@ import React, { useState, useEffect } from 'react';
 const DoctorList = ({ specificationId }) => {
   // STATE
   const [doctors, setDoctors] = useState([]);
-  const [error, setError] = useState([]);
-  
+  // const [error, setError] = useState([]);
+
+  // FETCH DATA
+  useEffect(() => {
+    const fetchDoctors = async () => {
+      try {
+        const response = await fetch(`http://localhost:3000/specifications/${specificationId}/doctors`);
+        setDoctors(response.data);
+      } catch (error) {
+        console.error('Error fetching doctors:', error);
+      }  
+      };
+      
+      fetchDoctors();
+    }, [specificationId]);
+
+};
