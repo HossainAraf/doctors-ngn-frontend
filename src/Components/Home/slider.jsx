f5394a706b76b20d76adf01f2d467bf7f1028a3b
Hossain <arafat.kd99@gmail.com>
2024-09-16 09:42:45 +0100

    Create Slider component where 'specifications' passed as a parameter & props"

--- src/Components/Home/slider.jsx
+++ src/Components/Home/slider.jsx
@@ -1,4 +1,34 @@
 import React from 'react';
 import Slider from 'react-slick';
+import { PropTypes } from 'prop-types';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+
+const DoctorSlider = ({ specifications }) => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 10,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
+  return (
+    <Slider {...settings}>
+      {specifications.slice(0, 3).map((spec) => (
+        <div key={spec.id}>
+          <h3>
+            Name: Dr.
+            {spec.name}
+          </h3>
+          <p>
+            Desc:
+            {spec.description}
+          </p>
+        </div>
+      ))}
+    </Slider>
+  );
+};
+
+export default DoctorSlider;
