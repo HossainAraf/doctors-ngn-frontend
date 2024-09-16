b596952ef1afda9d40aaf630fd15c285cd66e7fd
Hossain <arafat.kd99@gmail.com>
2024-09-16 04:35:35 +0100

    Refactor Home component to use specifications data from apiService file

--- src/Components/Home/home.jsx
+++ src/Components/Home/home.jsx
@@ -1,48 +1,35 @@
 // IMPORT
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-
-// API URL
-const apiUrl = 'http://localhost:3000/api/v1/specifications';
+import { fetchSpecifications } from '../../Services/apiService';
 
 // COMPONENT
 const Home = () => {
-  const [data, setData] = useState([]);
-  // const [error, setError] = useState(null);
+  const [specifications, setSpecifications] = useState([]);
 
-  //  FETCH DATA FROM API using useEffect async await
-  useEffect(
-    () => {
-      const fetchData = async () => {
-        try {
-          const response = await fetch(apiUrl);
-          console.log('Response:', response);
-          const data = await response.json();
-          setData(data);
-          console.log(data);
-        } catch (error) {
-          // setError(error);
-        }
-      };
-      fetchData();
-    },
-    [],
-  );
+  // FETCH DATA using useEffect async await
+  useEffect(() => {
+    const loadSpecifications = async () => {
+      const data = await fetchSpecifications();
+      setSpecifications(data);
+    };
+    loadSpecifications(); // Call the function
+  }, []); // Empty array to run only once
 
   // RETURN
   return (
     <div>
       <h1>Home</h1>
-      {data.map((item) => (
-        <div key={item.id}>
-          <Link to={`/doctors/${item.id}`}>
-            <button type="button">
-              View Doctors for:
-              {item.name}
+      {specifications.length > 0 ? (
+        <div>
+          {specifications.map((spec) => (
+            <button key={spec.id} type="button">
+              {spec.name}
             </button>
-          </Link>
+          ))}
         </div>
-      ))}
+      ) : (
+        <p>Loading specifications...</p>
+      )}
     </div>
   );
 };
