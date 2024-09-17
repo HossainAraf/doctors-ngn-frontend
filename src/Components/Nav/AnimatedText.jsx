bf3e996a86ebd73b67e1ad4b699973ac63f15daf
Hossain <arafat.kd99@gmail.com>
2024-09-17 07:41:51 +0100

    Update text animation properties

--- src/Components/Nav/AnimatedText.jsx
+++ src/Components/Nav/AnimatedText.jsx
@@ -2,19 +2,19 @@ import React from 'react';
 
 function AnimatedText() {
   return (
-    <div className="flex justify-center items-center h-screen bg-transparent">
+    <div className="flex justify-center items-center h-auto bg-transparent">
       <svg viewBox="0 0 1200 200" className="w-full">
         <text
           x="50%"
           y="50%"
           textAnchor="middle"
-          className="text-copy font-bold text-6xl fill-none stroke-white stroke-5 stroke-dasharray animate-stroke"
+          className="text-copy font-bold text-7xl fill-none stroke-white stroke-5 stroke-dasharray animate-stroke"
         >
-          <tspan className="text-copy1">Find your</tspan>
-          <tspan className="text-copy2">Doctors/</tspan>
-          <tspan className="text-copy3">Medical services</tspan>
-          <tspan className="text-copy4">from</tspan>
-          <tspan className="text-copy5">Naogaon</tspan>
+          <tspan className="text-copy1">U</tspan>
+          <tspan className="text-copy2">Te</tspan>
+          <tspan className="text-copy3">ch</tspan>
+          <tspan className="text-copy4">Dyna</tspan>
+          <tspan className="text-copy5">mics</tspan>
         </text>
       </svg>
     </div>
