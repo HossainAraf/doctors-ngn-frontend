72d814c0a3f54b0ef7e939e35cedcdb79a0b62b2
Hossain <arafat.kd99@gmail.com>
2024-09-23 05:20:25 +0100

    Replace props spreading with default props values

--- src/Components/BanglaInput.jsx
+++ src/Components/BanglaInput.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 
-const BanglaInput = ({ id, name, value, onChange, ...props }) => {
+const BanglaInput = ({
+  id, name, value, onChange, ...props
+}) => {
   const [inputValue, setInputValue] = useState(value || '');
   const BanglaRegex = /^[\u0980-\u09FF\s.,()\-!@#$%^&*+=?/{}[\]<>:;"'\\|~`_]+$/;
 
@@ -17,7 +19,10 @@ const BanglaInput = ({ id, name, value, onChange, ...props }) => {
         name={name}
         value={inputValue}
         onChange={handleChange}
-        {...props}
+        type={props.type || "text"}   // Defaulting to "text" if no type is passed
+        placeholder={props.placeholder || ''} // Defaulting to empty string
+        required={props.required || false} // Defaulting to false
+        disabled={props.disabled || false}// Defaulting to false
       />
       {inputValue.length > 0 && !BanglaRegex.test(inputValue) && (
         <p>Please enter Bangla text only.</p>
