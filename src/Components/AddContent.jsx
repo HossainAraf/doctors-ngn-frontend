9995bdfc94b22398874b98a6fb8c134bfd5f2417
Hossain <arafat.kd99@gmail.com>
2024-09-23 11:58:21 +0100

    Modify AddContent to render DoctorForm from this component

--- src/Components/AddContent.jsx
+++ src/Components/AddContent.jsx
@@ -1,9 +1,14 @@
 // IMPORTS
+import { Link } from 'react-router-dom';
+import Button from './Button';
 
 // CONPONENT
 const AddContent = () => (
   <div>
     <h1>Add Content</h1>
+    <Link to="/add-doctor">
+      <Button label="Add Doctor" />
+    </Link>
   </div>
 );
 
