5402b6aacd1c6a0e276671ad554bc8f63ae3165d
Hossain <arafat.kd99@gmail.com>
2024-09-24 12:47:12 +0100

    Update text & li positions in footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,9 +2,9 @@
 
 // COMPONENT
 const Footer = () => (
-  <div className="bg-black1">
+  <div className="bg-black1 text-white text-center">
     <p>&copy; 2024 UTech Dynamics. All rights reserved.</p>
-    <ul>
+    <ul className="flex justify-center space-x-3">
       <li><a href="https://www.linkedin.com/in/hossainaraf/" target="_blank" rel="noopener noreferrer">LinkedIn</a></li>
       <li><a href="https://www.facebook.com/hossainArafUtech" target="_blank" rel="noopener noreferrer">Facebook</a></li>
     </ul>
