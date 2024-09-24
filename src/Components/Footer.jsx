2fc4766ec2d78d6e27528fbc74735f7b2d9c5bcc
Hossain <arafat.kd99@gmail.com>
2024-09-24 12:14:35 +0100

    Add blank target to open new tab from anchor

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => (
   <>
     <p>&copy; 2024 UTech Dynamics. All rights reserved.</p>
     <ul>
-      <li><a href="https://www.facebook.com/hossainArafUtech">Facebook</a></li>
+      <li><a href="https://www.facebook.com/hossainArafUtech" target="_blank">Facebook</a></li>
     </ul>
   </>
 );
