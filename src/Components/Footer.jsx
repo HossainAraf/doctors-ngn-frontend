ecdb169f5b10ec848f0415ef433dd1ea99aabba9
Hossain <arafat.kd99@gmail.com>
2024-09-24 12:27:14 +0100

    Add noopener & noreferrer in reletion attribute in anchor to avoild secuirity risk for older browser

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -5,8 +5,8 @@ const Footer = () => (
   <>
     <p>&copy; 2024 UTech Dynamics. All rights reserved.</p>
     <ul>
-      <li><a href="https://www.linkedin.com/in/hossainaraf/" target="_blank">LinkedIn</a></li>
-      <li><a href="https://www.facebook.com/hossainArafUtech" target="_blank">Facebook</a></li>
+      <li><a href="https://www.linkedin.com/in/hossainaraf/" target="_blank" rel="noopener noreferrer">LinkedIn</a></li>
+      <li><a href="https://www.facebook.com/hossainArafUtech" target="_blank" rel="noopener noreferrer">Facebook</a></li>
     </ul>
   </>
 );
