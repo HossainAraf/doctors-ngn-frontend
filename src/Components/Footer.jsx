25c97dfa96090f7ea97432913901df61ef41a181
Hossain <arafat.kd99@gmail.com>
2024-09-24 12:33:35 +0100

    Change footer background color

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,13 +2,13 @@
 
 // COMPONENT
 const Footer = () => (
-  <>
+  <div className="bg-black1">
     <p>&copy; 2024 UTech Dynamics. All rights reserved.</p>
     <ul>
       <li><a href="https://www.linkedin.com/in/hossainaraf/" target="_blank" rel="noopener noreferrer">LinkedIn</a></li>
       <li><a href="https://www.facebook.com/hossainArafUtech" target="_blank" rel="noopener noreferrer">Facebook</a></li>
     </ul>
-  </>
+  </div>
 );
 
 // EXPORTS
