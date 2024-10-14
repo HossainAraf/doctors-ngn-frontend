4d437885c9d512fb97d02bc44e46c81d8dad82db
Hossain <arafat.kd99@gmail.com>
2024-10-14 09:34:26 +0100

    Add Link & Button to nevigate Feedback to FeedbackForm

--- src/Components/Feedback.jsx
+++ src/Components/Feedback.jsx
@@ -1,6 +1,8 @@
 // IMPORTS
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import { fetchFeedbacks } from '../Services/apiService';
+import Button from './Button';
 
 // COMPONENT
 const Feedback = () => {
@@ -20,6 +22,9 @@ const Feedback = () => {
   return (
     <div className="flex flex-col items-center">
       <h2 className="text-2xl font-bold mb-4">Feedback</h2>
+      <Link to="/add-feedback">
+        <Button label="Add new Feedback" className="bg-transparent hover:bg-indigo1 text-blue-700 font-semibold hover:text-white py-2 px-4 mx-1 my-1 border border-blue-500 hover:border-transparent rounded" />
+      </Link>
       <div className="w-1/2">
         {feedbacks.map((feedback) => (
           <div key={feedback.id} className="bg-white shadow-md p-4 mb-4">
