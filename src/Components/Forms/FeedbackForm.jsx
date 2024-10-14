2a8432669fdbadcdc7e13d6424989e867c960a83
Hossain <arafat.kd99@gmail.com>
2024-10-14 16:52:24 +0100

    Destructure form state & Assign nessecary props to avoid prop spreading

--- src/Components/Forms/FeedbackForm.jsx
+++ src/Components/Forms/FeedbackForm.jsx
@@ -2,17 +2,15 @@
 import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { fetchFeedbacks } from '../../Services/apiService';
-// import form from tailwindcss
 
 // COMPONENT
 const FeedbackForm = () => {
   // STATE
   const [feedbacks, setFeedbacks] = useState([]);
-  const { register, handleSubmit, errors } = useForm();
+  const { register, handleSubmit, formState: { errors } } = useForm();
 
   // HANDLERS
   const onSubmit = async (data) => {
-    // console.log(data);
     const newFeedback = {
       feedback: data.feedback,
       name: data.name,
@@ -32,13 +30,27 @@ const FeedbackForm = () => {
         className="flex flex-col items-center"
         onSubmit={handleSubmit(onSubmit)}
       >
+        <textarea
+          name="feedback"
+          placeholder="Feedback Message"
+          className="border border-gray-400 rounded-md p-2 mb-2"
+          ref={register('feedback', { required: true }).ref}
+          onChange={register('feedback', { required: true }).onChange}
+          onBlur={register('feedback', { required: true }).onBlur}
+        />
+        {errors.feedback && (
+          <span className="text-red-500 text-sm">Message is required</span>
+        )}
         <input
           type="text"
           name="name"
           placeholder="Name"
           className="border border-gray-400 rounded-md p-2 mb-2"
-          ref={register({ required: true })}
+          ref={register('name', { required: true }).ref}
+          onChange={register('name', { required: true }).onChange}
+          onBlur={register('name', { required: true }).onBlur}
         />
+
         {errors.name && (
           <span className="text-red-500 text-sm">Name is required</span>
         )}
@@ -47,20 +59,14 @@ const FeedbackForm = () => {
           name="email"
           placeholder="Email"
           className="border border-gray-400 rounded-md p-2 mb-2"
-          ref={register({ required: true })}
+          ref={register('email', { required: true }).ref}
+          onChange={register('email', { required: true }).onChange}
+          onBlur={register('email', { required: true }).onBlur}
         />
         {errors.email && (
           <span className="text-red-500 text-sm">Email is required</span>
         )}
-        <textarea
-          name="message"
-          placeholder="Message"
-          className="border border-gray-400 rounded-md p-2 mb-2"
-          ref={register({ required: true })}
-        />
-        {errors.message && (
-          <span className="text-red-500 text-sm">Message is required</span>
-        )}
+
         <button
           type="submit"
           className="bg-blue-500 text-white rounded-md p-2"
