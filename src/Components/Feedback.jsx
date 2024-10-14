a00dd3cf9ca956e0dce4168de0c12836931b06cb
Hossain <arafat.kd99@gmail.com>
2024-10-14 06:29:38 +0100

    Align indention

--- src/Components/Feedback.jsx
+++ src/Components/Feedback.jsx
@@ -9,15 +9,15 @@ const Feedback = () => {
   // STATE
   const [feedbacks, setFeedbacks] = useState([]);
 
-    // FETCH FEEDBACKS ON COMPONENT MOUNT
-    useEffect(() => {
-      const getFeedbacks = async () => {
-        const data = await fetchFeedbacks();
-        setFeedbacks(data);
-      };
-      getFeedbacks();
-    }, []);
-  
+  // FETCH FEEDBACKS ON COMPONENT MOUNT
+  useEffect(() => {
+    const getFeedbacks = async () => {
+      const data = await fetchFeedbacks();
+      setFeedbacks(data);
+    };
+    getFeedbacks();
+  }, []);
+
   // JSX
   return (
     <div className="flex flex-col items-center">
