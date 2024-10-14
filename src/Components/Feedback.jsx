45b8a1fea40802335abf468520e35728d9be68a6
Hossain <arafat.kd99@gmail.com>
2024-10-14 06:22:11 +0100

    Set useState hook to define state of feedback component

---
+++ src/Components/Feedback.jsx
@@ -0,0 +1,12 @@
+// Fetch all feedbacks from the database and display them in a table
+
+// IMPORTS
+import React, { useState } from 'react';
+
+// COMPONENT
+const Feedback = () => {
+  // STATE
+  const [feedbacks, setFeedbacks] = useState([]);
+
+
+};
