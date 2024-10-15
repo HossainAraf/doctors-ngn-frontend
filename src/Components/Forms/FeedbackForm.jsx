66fbad3c43f086bdd7336d7cbdb6c17f8bc55686
Hossain <arafat.kd99@gmail.com>
2024-10-15 16:24:57 +0100

    Add clear feddback form functionality after each submission

--- src/Components/Forms/FeedbackForm.jsx
+++ src/Components/Forms/FeedbackForm.jsx
@@ -1,42 +1,57 @@
-// IMPORTS
 import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { createFeedback } from '../../Services/apiService';
 
-// COMPONENT
 const FeedbackForm = () => {
-  // STATE
   const [feedbacks, setFeedbacks] = useState([]);
-  const { register, handleSubmit, formState: { errors } } = useForm();
+  const [isSubmitted, setIsSubmitted] = useState(false);
+  const {
+    register, handleSubmit, reset, formState: { errors },
+  } = useForm();
 
-  // HANDLERS
+  const clearForm = () => {
+    reset();
+    setIsSubmitted(false);
+  };
+
   const onSubmit = async (data) => {
     const newFeedback = {
       feedback: data.feedback,
       name: data.name,
       email: data.email,
     };
+
     const response = await createFeedback(newFeedback);
     if (response) {
       setFeedbacks([...feedbacks, response]);
+      setIsSubmitted(true);
+      // setTimeout(() => clearForm(), 3000);
     }
   };
 
-  // JSX
   return (
-    <div className="flex flex-col items-center">
+    <div className="feedback-form flex flex-col max-w-sm mx-auto">
       <h2 className="text-2xl font-bold mb-4">Feedback Form</h2>
-      <form
-        className="flex flex-col items-center"
-        onSubmit={handleSubmit(onSubmit)}
-      >
+      {isSubmitted && (
+        <div className="alert alert-success" role="alert">
+          <p>Your Feedback Submitted Successfully. Thanks!</p>
+          <button
+            type="button"
+            className="bg-indigo-500 text-white hover:bg-transparent hover:text-blue-700 p-2 rounded"
+            onClick={clearForm}
+          >
+            CLOSE
+          </button>
+        </div>
+      )}
+      <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col">
         <textarea
           name="feedback"
           placeholder="Feedback Message"
-          className="border border-gray-400 rounded-md p-2 mb-2"
-          ref={register('feedback', { required: true }).ref}
-          onChange={register('feedback', { required: true }).onChange}
-          onBlur={register('feedback', { required: true }).onBlur}
+          className="border border-gray-400 rounded-md p-2 mb-2 min-h-[100px]" // Minimum height for better responsiveness
+          ref={register('feedback', { required: true })}
+          onChange={register('feedback').onChange}
+          onBlur={register('feedback').onBlur}
         />
         {errors.feedback && (
           <span className="text-red-500 text-sm">Message is required</span>
@@ -46,11 +61,10 @@ const FeedbackForm = () => {
           name="name"
           placeholder="Name"
           className="border border-gray-400 rounded-md p-2 mb-2"
-          ref={register('name', { required: true }).ref}
-          onChange={register('name', { required: true }).onChange}
-          onBlur={register('name', { required: true }).onBlur}
+          ref={register('name', { required: true })}
+          onChange={register('name').onChange}
+          onBlur={register('name').onBlur}
         />
-
         {errors.name && (
           <span className="text-red-500 text-sm">Name is required</span>
         )}
@@ -59,18 +73,14 @@ const FeedbackForm = () => {
           name="email"
           placeholder="Email"
           className="border border-gray-400 rounded-md p-2 mb-2"
-          ref={register('email', { required: true }).ref}
-          onChange={register('email', { required: true }).onChange}
-          onBlur={register('email', { required: true }).onBlur}
+          ref={register('email', { required: true })}
+          onChange={register('email').onChange}
+          onBlur={register('email').onBlur}
         />
         {errors.email && (
           <span className="text-red-500 text-sm">Email is required</span>
         )}
-
-        <button
-          type="submit"
-          className="bg-blue-500 text-white rounded-md p-2"
-        >
+        <button type="submit" className="bg-blue-500 text-white rounded-md p-2">
           Submit
         </button>
       </form>
@@ -78,5 +88,4 @@ const FeedbackForm = () => {
   );
 };
 
-// EXPORTS
 export default FeedbackForm;
