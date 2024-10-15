c76f7c232442a9d430f0292bc29c5199c1e61a08
Hossain <arafat.kd99@gmail.com>
2024-10-15 17:10:27 +0100

    Update feedback form style

--- src/Components/Forms/FeedbackForm.jsx
+++ src/Components/Forms/FeedbackForm.jsx
@@ -34,8 +34,8 @@ const FeedbackForm = () => {
       <h2 className="text-2xl font-bold mb-4">Feedback Form</h2>
       {isSubmitted && (
         <div className="alert alert-success" role="alert">
-          <p>Your Feedback Submitted Successfully. Thanks!</p>
-          <button type="button" className="bg-indigo1 text-white hover:bg-transparent hover:text-blue-700 p-1 mb-2 rounded" onClick={clearForm}>
+          <p className="text-green-900 text-xl font-bold">Your Feedback Submitted Successfully. Thanks!</p>
+          <button type="button" className="bg-indigo1 text-white hover:bg-transparent hover:text-blue-700 p-1 mb-2 border border-transparent hover:border-blue-500 rounded" onClick={clearForm}>
             Close
           </button>
         </div>
@@ -46,40 +46,40 @@ const FeedbackForm = () => {
       >
         <textarea
           name="feedback"
-          placeholder="Feedback Message"
-          className="border border-gray-400 rounded-md p-2 mb-2 min-h-[100px]"
+          placeholder="Please Write Your Message"
+          className="border border-gray-400 rounded-md p-2 min-h-[100px]"
           ref={register('feedback', { required: true }).ref}
           onChange={register('feedback', { required: true }).onChange}
           onBlur={register('feedback', { required: true }).onBlur}
         />
         {errors.feedback && (
-          <span className="text-red-500 text-sm">Message is required</span>
+          <span className="text-red-500 text-sm mb-2">Message is required</span>
         )}
         <input
           type="text"
           name="name"
           placeholder="Name"
-          className="border border-gray-400 rounded-md p-2 mb-2"
+          className="border border-gray-400 rounded-md p-2"
           ref={register('name', { required: true }).ref}
           onChange={register('name', { required: true }).onChange}
           onBlur={register('name', { required: true }).onBlur}
         />
         {errors.name && (
-          <span className="text-red-500 text-sm">Name is required</span>
+          <span className="text-red-500 text-sm mb-2">Name is required</span>
         )}
         <input
           type="email"
           name="email"
           placeholder="Email"
-          className="border border-gray-400 rounded-md p-2 mb-2"
+          className="border border-gray-400 rounded-md p-2"
           ref={register('email', { required: true }).ref}
           onChange={register('email', { required: true }).onChange}
           onBlur={register('email', { required: true }).onBlur}
         />
         {errors.email && (
-          <span className="text-red-500 text-sm">Email is required</span>
+          <span className="text-red-500 text-sm mb-2">Email is required</span>
         )}
-        <button type="submit" className="bg-indigo1 text-white rounded-md p-2">
+        <button type="submit" className="bg-indigo1 text-white hover:bg-transparent hover:text-blue-500 border border-transparent hover:border-blue-500 rounded-md p-2">
           Submit
         </button>
       </form>
