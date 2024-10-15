17eebaa964df2c3f945f808af54a71992f3ef5e9
Hossain <arafat.kd99@gmail.com>
2024-10-15 16:54:58 +0100

    Update Feedback form style

--- src/Components/Forms/FeedbackForm.jsx
+++ src/Components/Forms/FeedbackForm.jsx
@@ -9,49 +9,47 @@ const FeedbackForm = () => {
     register, handleSubmit, reset, formState: { errors },
   } = useForm();
 
-  const clearForm = () => {
-    reset();
-    setIsSubmitted(false);
-  };
-
   const onSubmit = async (data) => {
     const newFeedback = {
       feedback: data.feedback,
       name: data.name,
       email: data.email,
     };
-
     const response = await createFeedback(newFeedback);
     if (response) {
       setFeedbacks([...feedbacks, response]);
       setIsSubmitted(true);
-      // setTimeout(() => clearForm(), 3000);
+      // setTimeout(() => clearForm(), 1000);
     }
   };
 
+  const clearForm = () => {
+    reset();
+    setIsSubmitted(false);
+  };
+
   return (
-    <div className="feedback-form flex flex-col max-w-sm mx-auto">
+    <div className="flex flex-col max-w-sm mx-auto">
       <h2 className="text-2xl font-bold mb-4">Feedback Form</h2>
       {isSubmitted && (
         <div className="alert alert-success" role="alert">
           <p>Your Feedback Submitted Successfully. Thanks!</p>
-          <button
-            type="button"
-            className="bg-indigo1 text-white hover:bg-transparent hover:text-blue-700 p-1 mb-2 rounded"
-            onClick={clearForm}
-          >
-            CLOSE
+          <button type="button" className="bg-indigo1 text-white hover:bg-transparent hover:text-blue-700 p-1 mb-2 rounded" onClick={clearForm}>
+            Close
           </button>
         </div>
       )}
-      <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col">
+      <form
+        className="flex flex-col"
+        onSubmit={handleSubmit(onSubmit)}
+      >
         <textarea
           name="feedback"
           placeholder="Feedback Message"
-          className="border border-gray-400 rounded-md p-2 mb-2 min-h-[100px]" // Minimum height for better responsiveness
-          ref={register('feedback', { required: true })}
-          onChange={register('feedback').onChange}
-          onBlur={register('feedback').onBlur}
+          className="border border-gray-400 rounded-md p-2 mb-2 min-h-[100px]"
+          ref={register('feedback', { required: true }).ref}
+          onChange={register('feedback', { required: true }).onChange}
+          onBlur={register('feedback', { required: true }).onBlur}
         />
         {errors.feedback && (
           <span className="text-red-500 text-sm">Message is required</span>
@@ -61,9 +59,9 @@ const FeedbackForm = () => {
           name="name"
           placeholder="Name"
           className="border border-gray-400 rounded-md p-2 mb-2"
-          ref={register('name', { required: true })}
-          onChange={register('name').onChange}
-          onBlur={register('name').onBlur}
+          ref={register('name', { required: true }).ref}
+          onChange={register('name', { required: true }).onChange}
+          onBlur={register('name', { required: true }).onBlur}
         />
         {errors.name && (
           <span className="text-red-500 text-sm">Name is required</span>
@@ -73,9 +71,9 @@ const FeedbackForm = () => {
           name="email"
           placeholder="Email"
           className="border border-gray-400 rounded-md p-2 mb-2"
-          ref={register('email', { required: true })}
-          onChange={register('email').onChange}
-          onBlur={register('email').onBlur}
+          ref={register('email', { required: true }).ref}
+          onChange={register('email', { required: true }).onChange}
+          onBlur={register('email', { required: true }).onBlur}
         />
         {errors.email && (
           <span className="text-red-500 text-sm">Email is required</span>
