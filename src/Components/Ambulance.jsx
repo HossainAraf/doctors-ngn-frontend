439c2aebece4c29a7a4f86355e0489fc31b71901
Hossain <arafat.kd99@gmail.com>
2024-10-16 16:34:11 +0600

    Add basic style to Ambulance

--- src/Components/Ambulance.jsx
+++ src/Components/Ambulance.jsx
@@ -1,8 +1,12 @@
 // COMPONENT
 const Ambulance = () => (
   <div>
-    <h1>অ্যাম্বুলেন্স সেবা</h1>
+    <h1 className="flex flex-col text-center">অ্যাম্বুলেন্স সেবা</h1>
     <p>This page is under construction</p>
+    <div className="flex flex-col p-3 shadow">
+      <p>নামঃ&nbsp;কিছু একটা নাম</p>
+      <h5>যোগাযোগঃ&nbsp;০১৭........</h5>
+    </div>
   </div>
 );
 
